0ecce0d49a56466485b143618f73242e7e59354e
hyeseon0105 <hyesun0536@gmail.com>
2025-05-13 17:26:05 +0900

    상품 문의

--- src/main/java/com/project/msy/qna/dto/QuestionRequest.java
+++ src/main/java/com/project/msy/qna/dto/QuestionRequest.java
@@ -1,6 +1,7 @@
 package com.project.msy.qna.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,9 +11,13 @@ import lombok.Setter;
 @Getter
 @Setter
 public class QuestionRequest {
+
     @NotBlank(message = "제목을 입력해주세요")
     private String title;
 
     @NotBlank(message = "내용을 입력해주세요")
     private String content;
+
+    @NotNull(message = "상품 ID는 필수입니다") // ✅ productId는 반드시 있어야 함
+    private Long productId;
 }
